da8e278c1568bbe11679e770a3218ff233c0adad
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-10-31 17:23:52 +0100

    Added save on disable

--- src/main/java/buttondevteam/core/MainPlugin.java
+++ src/main/java/buttondevteam/core/MainPlugin.java
@@ -1,5 +1,7 @@
 package buttondevteam.core;
 
+import java.util.Map.Entry;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -7,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import buttondevteam.discordplugin.EventExceptionDiscordSender;
 import buttondevteam.lib.EventExceptionHandler;
+import buttondevteam.lib.TBMCPlayer;
 
 public class MainPlugin extends JavaPlugin {
 	public static MainPlugin Instance;
@@ -14,6 +17,7 @@ public class MainPlugin extends JavaPlugin {
 	private PluginDescriptionFile pdfFile;
 	private Logger logger;
 
+	@Override
 	public void onEnable() {
 		// Logs "Plugin Enabled", registers commands
 		Instance = this;
@@ -22,6 +26,15 @@ public class MainPlugin extends JavaPlugin {
 
 		logger.info(pdfFile.getName() + " has been Enabled (V." + pdfFile.getVersion() + ").");
 		EventExceptionHandler.registerEvents(new PlayerListener(), this, new EventExceptionDiscordSender());
+	}
+
+	@Override
+	public void onDisable() {
+		logger.info("Saving player data...");
+		for (Entry<UUID, TBMCPlayer> entry : TBMCPlayer.getLoadedPlayers().entrySet()) {
+			TBMCPlayer.savePlayer(entry.getValue());
+		}
+		logger.info("Player data saved.");
 	}
 
 }
