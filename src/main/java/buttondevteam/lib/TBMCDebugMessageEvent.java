1adc2f1b4c3c1f8b65fecad716f7a47135b97a49
alisolarflare <alisunderstorm@gmail.com>
2016-11-21 14:21:14 -0500

    Created DebugMessages for Discord
    Similar, and copypasted from TMBCException Event, TBMCDebugMessageEvent,
    should ideally send a plain string message to TBMC Devs II -
    debug-channel

---
+++ src/main/java/buttondevteam/lib/TBMCDebugMessageEvent.java
@@ -0,0 +1,49 @@
+package buttondevteam.lib;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class TBMCDebugMessageEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	private String message;
+	private boolean sent;
+
+	public TBMCDebugMessageEvent(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Gets the message (where did this exception occur, etc.)
+	 * 
+	 * @return The message
+	 */
+	public String getDebugMessage() {
+		return message;
+	}
+
+	/**
+	 * Gets if this event was handled
+	 * 
+	 * @return True if it was handled
+	 */
+	public boolean isSent() {
+		return sent;
+	}
+
+	/**
+	 * Flags the event as handled
+	 */
+	public void setSent() {
+		this.sent = true;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+}
