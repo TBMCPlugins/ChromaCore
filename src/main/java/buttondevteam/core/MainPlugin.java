a9b0e98e07788111dd7d7e423a99ff8eb93d3761
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-11-25 23:39:56 +0100

    Started using databases, WIP

--- src/main/java/buttondevteam/core/MainPlugin.java
+++ src/main/java/buttondevteam/core/MainPlugin.java
@@ -4,11 +4,16 @@ import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+import javax.persistence.PersistenceException;
+
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import buttondevteam.lib.CPlayer;
 import buttondevteam.lib.TBMCCoreAPI;
 import buttondevteam.lib.TBMCPlayer;
+import buttondevteam.lib.db.CData;
+import buttondevteam.lib.db.DataManager;
 
 public class MainPlugin extends JavaPlugin {
 	public static MainPlugin Instance;
@@ -23,9 +28,28 @@ public class MainPlugin extends JavaPlugin {
 		pdfFile = getDescription();
 		logger = getLogger();
 
+		setupDatabase();
+		DataManager.setDatabase(getDatabase());
+		final UUID cid = UUID.randomUUID();
+		final UUID mcid = UUID.randomUUID();
+		System.out.println(cid);
+		System.out.println(mcid);
+		System.out.println("----");
+		DataManager.save(new CPlayer(cid, mcid));
+		System.out.println("----");
+		System.out.println(DataManager.load(CPlayer.class, cid).getMinecraftID());
 		logger.info(pdfFile.getName() + " has been Enabled (V." + pdfFile.getVersion() + ").");
 		TBMCCoreAPI.RegisterEventsForExceptions(new PlayerListener(), this);
 	}
+	
+    private void setupDatabase() {
+        try {
+            getDatabase().find(CPlayer.class).findRowCount();
+        } catch (PersistenceException ex) {
+            System.out.println("Installing database for ButtonCore due to first time usage");
+            installDDL();
+        }
+    }
 
 	@Override
 	public void onDisable() {
@@ -35,5 +59,4 @@ public class MainPlugin extends JavaPlugin {
 		}
 		logger.info("Player data saved.");
 	}
-
 }
