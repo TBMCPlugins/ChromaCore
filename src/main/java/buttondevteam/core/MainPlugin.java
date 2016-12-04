773bb668081f13351bdc6ec62b19a57d8b178828
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-12-04 19:17:14 +0100

    Now one won't see subcmds they don't have access to

--- src/main/java/buttondevteam/core/MainPlugin.java
+++ src/main/java/buttondevteam/core/MainPlugin.java
@@ -5,13 +5,16 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import buttondevteam.lib.TBMCCoreAPI;
 import buttondevteam.lib.TBMCPlayer;
+import net.milkbowl.vault.permission.Permission;
 
 public class MainPlugin extends JavaPlugin {
 	public static MainPlugin Instance;
+	public static Permission permission;
 
 	private PluginDescriptionFile pdfFile;
 	private Logger logger;
@@ -22,7 +25,7 @@ public class MainPlugin extends JavaPlugin {
 		Instance = this;
 		pdfFile = getDescription();
 		logger = getLogger();
-
+		setupPermissions();
 		logger.info(pdfFile.getName() + " has been Enabled (V." + pdfFile.getVersion() + ").");
 		TBMCCoreAPI.RegisterEventsForExceptions(new PlayerListener(), this);
 	}
@@ -34,5 +37,14 @@ public class MainPlugin extends JavaPlugin {
 			TBMCPlayer.savePlayer(entry.getValue());
 		}
 		logger.info("Player data saved.");
+	}
+
+	private boolean setupPermissions() {
+		RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager()
+				.getRegistration(Permission.class);
+		if (permissionProvider != null) {
+			permission = permissionProvider.getProvider();
+		}
+		return (permission != null);
 	}
 }
