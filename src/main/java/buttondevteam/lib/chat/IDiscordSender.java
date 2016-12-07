381739456c405474d6133111fc9dd8db46f329e8
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-12-07 17:49:06 +0100

    Added IDiscordSender to distinguish

---
+++ src/main/java/buttondevteam/lib/chat/IDiscordSender.java
@@ -0,0 +1,12 @@
+package buttondevteam.lib.chat;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ * DiscordSenderBase implements it
+ * @author Norbi
+ *
+ */
+public interface IDiscordSender extends CommandSender {
+
+}
