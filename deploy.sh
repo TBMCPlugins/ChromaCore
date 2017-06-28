765610e6e3a5784449818b9ca5fec73c7368429c
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-06-28 18:02:27 +0200

    Using the yes command to answer everything

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo Found file: $FILENAME
 if [ $1 = 'production' ]; then
 echo Production mode
 echo $UPLOAD_KEY > upload_key
-scp -i upload_key $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
+yes | scp -i upload_key $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
 fi
