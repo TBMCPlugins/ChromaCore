39ee9ba498096008f6cd815b026b64b59c08faca
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-06-28 18:12:24 +0200

    That didn't work so try this

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ echo Found file: $FILENAME
 if [ $1 = 'production' ]; then
 echo Production mode
 echo $UPLOAD_KEY > upload_key
-yes | scp -i upload_key $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
+yes | scp -B  -i upload_key -o StrictHostKeyChecking=no $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
 fi
+  
