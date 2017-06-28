6006e995b96f089ae70054eb0bd2cda4022ab7db
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-06-28 18:20:48 +0200

    That kinda worked but need to make the key private

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo Found file: $FILENAME
 if [ $1 = 'production' ]; then
 echo Production mode
 echo $UPLOAD_KEY > upload_key
+chmod 400 upload_key
 yes | scp -B  -i upload_key -o StrictHostKeyChecking=no $FILENAME travis@server.figytuna.com:/minecraft/main/plugins
 fi
   
