509fe99294442b9c784824862e7692f6e1b1bdc5
NorbiPeti <szatmari.norbert.peter@gmail.com>
2018-01-08 21:48:45 +0100

    Fixed for Spigot, added error reporting

--- src/main/java/buttondevteam/ChunkArchive/Main.java
+++ src/main/java/buttondevteam/ChunkArchive/Main.java
@@ -11,31 +11,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkPopulateEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import buttondevteam.lib.TBMCCoreAPI;
 import net.minecraft.server.v1_12_R1.ChunkRegionLoader;
 import net.minecraft.server.v1_12_R1.ExceptionWorldConflict;
 
-public class Main extends JavaPlugin implements Listener
-{
-	public void onEnable()
-	{
-		getServer().getPluginManager().registerEvents(this, this);
+public class Main extends JavaPlugin implements Listener {
+	public void onEnable() {
+		TBMCCoreAPI.RegisterEventsForExceptions(this, this);
 	}
 	/*------------------------------------------------------------*/
-	
-	private final ChunkRegionLoader chunkRegionLoader = 
-			
-			new ChunkRegionLoader(this.getDataFolder(), ((CraftServer) Bukkit.getServer())
-														.getServer()
-														.dataConverterManager);
-	
+
+	private final ChunkRegionLoader chunkRegionLoader =
+
+			new ChunkRegionLoader(this.getDataFolder(),
+					((CraftServer) Bukkit.getServer()).getServer().dataConverterManager);
+
 	/*------------------------------------------------------------*/
-	
+
 	@EventHandler
 	public void onChunkPopulate(ChunkPopulateEvent event) throws IOException, 
 																 ExceptionWorldConflict
 	{
 		if (event.getWorld().getName().equals("world"))
 			chunkRegionLoader.saveChunk(((CraftWorld) event.getWorld()).getHandle(),
-								((CraftChunk) event.getChunk()).getHandle());
+								((CraftChunk) event.getChunk()).getHandle(), false);
 	}
 }
